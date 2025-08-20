88618f3ef9cbd38b838b56e29502d9a354873781
bladdezz <bladdezz@bladenet.xyz>
2025-08-20 15:07:02 -0400

    Initial Commit

---
+++ SpyPaste/SpyPasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpyPasteApp.swift
+//  SpyPaste
+//
+//  Created by nathan on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SpyPasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpyPasteTests/SpyPasteTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpyPasteTests.swift
+//  SpyPasteTests
+//
+//  Created by nathan on 8/20/25.
+//
+
+import Testing
+@testable import SpyPaste
+
+struct SpyPasteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
